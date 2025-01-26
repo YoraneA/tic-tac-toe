937b217c6ce37e3b18c7075cac852418b358eb76
Yorane Abbou <yorane.a@gmail.com>
2025-01-26 09:53:56 +0200

    Show 'X' on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import {useState} from "react";
+
 export default function Board() {
-    return (
-        <>
-            <div className="board-row">
-                <Square value="1"/>
-                <Square value="2"/>
-                <Square value="3"/>
-            </div>
-            <div className="board-row">
-                <Square value="4"/>
-                <Square value="5"/>
-                <Square value="6"/>
-            </div>
-            <div className="board-row">
-                <Square value="7"/>
-                <Square value="8"/>
-                <Square value="9"/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
+    </>
+  );
 }
 
-function Square({value}) {
-    return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null)
+
+  function handleClick() {
+    setValue('X')
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
